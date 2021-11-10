796aa153dfa01d5806a1939a0fb9821cef940c0f
Sameer <smalik@wpi.edu>
2021-11-10 04:40:05 -0500

    chages for this

--- src/component/Code/TreeSpace/TreeNode.js
+++ src/component/Code/TreeSpace/TreeNode.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 import { connect } from 'react-redux';
-
+import AWS from 'aws-sdk'
 import classes from './TreeNode.module.css';
 import { RightOutlined, PlusOutlined, DownOutlined, DeleteOutlined,FontColorsOutlined } from '@ant-design/icons';
 import Tree from './Tree';
@@ -10,13 +10,15 @@ import { Upload } from 'antd';
 import * as CodeAction from '../../../store/action/code';
 
 
-import { UploadFile } from 'antd';
+
 
 
 
 
 
 const TreeNode = (props) => {
+    const [progress , setProgress] = useState(0);
+const [selectedFile, setSelectedFile] = useState(null);
     const [childVisible, setChildVisibility] = useState(false);
     const addButtonHidden = props.node.type!=="algorithm"&& (!props.node.type.includes('problem'))&& (!props.node.type.includes('benchmark'))? false : true;
     const hasChild = props.node.children ? true : false;
@@ -25,6 +27,45 @@ const TreeNode = (props) => {
     const ImplementationItem = props.node.type==="algorithm_implementations"||props.node.type==="algorithm_problem"?true:false;
     const deleteButtonHidden = true;
     // const deleteButtonHidden = (!node.type.includes('benchmark'))? true : false;
+    const S3_BUCKET ='cs-509-implementations';
+    const REGION ='us-east-2';
+
+
+AWS.config.update({
+    accessKeyId: 'AKIA3A3Z************',
+    secretAccessKey: '86WxkHcq6*************'
+})
+
+const myBucket = new AWS.S3({
+    params: { Bucket: S3_BUCKET},
+    region: REGION,
+})
+    let handleFileInput =(e)=>{
+        setSelectedFile(e.target.files[0]);
+    }
+    const uploadFile = (file, treeData,key) => {
+
+        const params = {
+            ACL: 'public-read',
+            Body: file,
+            Bucket: S3_BUCKET,
+            Key: file.name
+        };
+
+        myBucket.putObject(params)
+            .on('httpUploadProgress', (evt) => {
+                setProgress(Math.round((evt.loaded / evt.total) * 100))
+                
+                if(evt.loaded==100)
+                console.log("done")
+            })
+            .send((err) => {
+                if (!err) console.log("done deplo")
+             let   url = "https://" + S3_BUCKET+".s3." + REGION  + ".amazonaws.com/"+ encodeURI(file.name)
+                console.log(url)
+              //  props.addImplementationChild(treeData,key, url);
+            })
+    }
 
     let editItem = (
         <div className={classes.toggler + ' ' + (childVisible ? classes.active : '')}>
@@ -35,8 +76,26 @@ const TreeNode = (props) => {
            
             {/* <p className={classes.editTitle}>{props.node.title}</p> */}
             <Input onChange={({ target: { value } })=>props.modifyTree(props.treeData,props.node.key,value)} className={AlgorithmItem?classes.editTitleAlgorithm:classes.editTitle} placeholder="Basic usage" defaultValue={props.node.title} size="small"/>
-             <div  className = {ImplementationItem? classes.upload:classes.hideUpload}><Upload className={classes.uploadButton} action={'/upload'}  >
-             <Button>Upload</Button></Upload></div>
+             <div  className = {ImplementationItem? classes.upload:classes.hideUpload}>
+                 {/* <Upload className={classes.uploadButton} onChange={(response) => {
+            if (response.file.status !== 'uploading') {
+              console.log(response.file, response.fileList);
+            }
+            if (response.file.status === 'done') {
+                console.log("issue")
+            //   message.success(`${response.file.name} 
+            //                    file uploaded successfully`);
+            } else if (response.file.status === 'error') {
+            //   message.error(`${response.file.name} 
+            //                  file upload failed.`);
+            console.log("error")
+            }
+          }} action={'/upload'}  > */}
+             {/* <Button>Upload</Button> */}
+             {/* </Upload> */}
+             <input type="file" onChange={handleFileInput}/>
+        <button onClick={() => uploadFile(selectedFile, props.treeData,props.key)}> Upload to S3</button>
+             </div>
         </div>
     )
    
