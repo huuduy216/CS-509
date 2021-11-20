da9224d855d3e7330d1f8dc45606435cad54ae69
jijiaqi16 <1160117897j@gmail.com>
2021-11-20 00:28:35 -0500

    11/20/2021

--- src/axios/axios-local.js
+++ src/axios/axios-local.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:'http://localhost:8080',
-    // baseURL:'http://ec2-3-144-85-145.us-east-2.compute.amazonaws.com:8080/'
+    //local
+    // baseURL:'http://localhost:8080'
+
+    //group
+    baseURL:'http://ec2-3-144-85-145.us-east-2.compute.amazonaws.com:8080/'
+
+    //jjq
+    // baseURL:'http:// ec2-3-144-140-189.us-east-2.compute.amazonaws.com:8080/'
+
 
 });
 
